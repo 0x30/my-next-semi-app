c5fcb750f94017eafad7debdc273bca6b7f58e61
Gavin <200739491@qq.com>
2022-09-26 20:42:41 +0800

    feat: https://semi.design/zh-CN/start/getting-started 按教程配置项目

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+// next.config.js
+const semi = require("@douyinfe/semi-next").default({
+  /* the extension options */
+});
+
+module.exports = semi({
   reactStrictMode: true,
   swcMinify: true,
-}
-
-module.exports = nextConfig
+});
